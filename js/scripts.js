872f0cdbcd7bfe9d8403f60da3c531144cc6c592
Evan Bishop and Liam Stabeno <evan@>
2017-02-14 14:29:54 -0800

    test

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("button#Jerrybutton").click(function() {
+    $("#Jerry").prepend("<li>Hello Jesus</li>");
+  });
+  $("button#Jesusbutton").click(function() {
+    $("#Jesus").prepend("<li>Sup Jerbear!</li>");
+  });
+});
